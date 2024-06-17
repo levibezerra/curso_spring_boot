78c99337a3a18bcdd857e2ef4e71707b318a8329
levibezerra <levilimabezerra20@gmail.com>
2024-06-17 12:30:52 -0300

    Adicionando recurso para criar um novo usuario

--- src/main/java/com/levi/demo_park_api/service/UsuarioService.java
+++ src/main/java/com/levi/demo_park_api/service/UsuarioService.java
@@ -1,8 +1,10 @@
 package com.levi.demo_park_api.service;
 
+import com.levi.demo_park_api.entity.Usuario;
 import com.levi.demo_park_api.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -10,4 +12,9 @@ import org.springframework.stereotype.Service;
 public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
+
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
 }
