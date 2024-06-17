26ba1a6dff873e68f6e59beec909730fa197e492
levibezerra <levilimabezerra20@gmail.com>
2024-06-17 11:23:29 -0300

    Adicionando UsuarioRepository, UsuarioService e UsuarioController

---
+++ src/main/java/com/levi/demo_park_api/service/UsuarioService.java
@@ -0,0 +1,13 @@
+package com.levi.demo_park_api.service;
+
+import com.levi.demo_park_api.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+
+public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+}
