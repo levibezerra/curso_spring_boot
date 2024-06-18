f091bced458da8f52902533a2cdda46abe2afd5e
levibezerra <levilimabezerra20@gmail.com>
2024-06-18 12:49:51 -0300

    Criando DTO para criar um novo usuário

--- src/main/java/com/levi/demo_park_api/web/controller/UsuarioController.java
+++ src/main/java/com/levi/demo_park_api/web/controller/UsuarioController.java
@@ -2,6 +2,9 @@ package com.levi.demo_park_api.web.controller;
 
 import com.levi.demo_park_api.entity.Usuario;
 import com.levi.demo_park_api.service.UsuarioService;
+import com.levi.demo_park_api.web.dto.UsuarioCreateDto;
+import com.levi.demo_park_api.web.dto.UsuarioResponseDto;
+import com.levi.demo_park_api.web.dto.mapper.UsuarioMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,9 +21,9 @@ public class UsuarioController{
     private final UsuarioService usuarioService;
 
     @PostMapping
-    public ResponseEntity<Usuario> create(@RequestBody Usuario usuario) {
-        Usuario user = usuarioService.salvar(usuario);
-        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    public ResponseEntity<UsuarioResponseDto> create(@RequestBody UsuarioCreateDto createDto) {
+        Usuario user = usuarioService.salvar(UsuarioMapper.toUsuario(createDto));
+        return ResponseEntity.status(HttpStatus.CREATED).body(UsuarioMapper.toDto(user));
     }
 
     @GetMapping("/{id}")
