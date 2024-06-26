33c677b27af14ccaa2d56c9613a03c54079a083d
levibezerra <levilimabezerra20@gmail.com>
2024-06-26 19:22:24 -0300

    Ajustes para o processo de autenticação de login

--- src/main/java/com/levi/demo_park_api/service/UsuarioService.java
+++ src/main/java/com/levi/demo_park_api/service/UsuarioService.java
@@ -6,6 +6,7 @@ import com.levi.demo_park_api.exception.PasswordInvalidException;
 import com.levi.demo_park_api.exception.UsernameUniqueViolationException;
 import com.levi.demo_park_api.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,10 +18,12 @@ import java.util.List;
 public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
     public Usuario salvar(Usuario usuario) {
         try {
+            usuario.setPassword(passwordEncoder.encode(usuario.getPassword()));
             return usuarioRepository.save(usuario);
         }catch(org.springframework.dao.DataIntegrityViolationException ex) {
             throw new UsernameUniqueViolationException(String.format("Username %s já cadastrado", usuario.getUsername()));
@@ -41,10 +44,10 @@ public class UsuarioService {
         }
 
         Usuario user = buscarPorId(id);
-        if(!user.getPassword().equals(senhaAtual)) {
+        if(!passwordEncoder.matches(senhaAtual, user.getPassword())) {
             throw new PasswordInvalidException("Sua senha não confere!.");
         }
-        user.setPassword(novaSenha);
+        user.setPassword(passwordEncoder.encode(novaSenha));
         return user;
     }
 
