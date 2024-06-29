5e0a845d7c0be5c757811b555b96890d9653a855
levibezerra <levilimabezerra20@gmail.com>
2024-06-28 23:18:15 -0300

    Adicionando uma mundança no método doFilterInternal

--- src/main/java/com/levi/demo_park_api/jwt/JwtAuthorizationFilter.java
+++ src/main/java/com/levi/demo_park_api/jwt/JwtAuthorizationFilter.java
@@ -25,7 +25,7 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
         final String token = request.getHeader(JwtUtils.JWT_AUTHORIZATION);
-        if(token == null || token.startsWith(JwtUtils.JWT_BEARER)) {
+        if(token == null || !token.startsWith(JwtUtils.JWT_BEARER)) {
             log.info("JWT Token está nulo, vazio ou não iniciado com 'Bearer '.");
             filterChain.doFilter(request, response);
             return;
