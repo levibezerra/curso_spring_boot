a3725d3f6afbefc0843fbc8e3ad360a61e6bcfbb
levibezerra <levilimabezerra20@gmail.com>
2024-07-05 18:11:38 -0300

    Testando com autenticação o recurso buscar usuário pelo id com status 200

--- src/test/resources/sql/usuarios/usuarios-insert.sql
+++ src/test/resources/sql/usuarios/usuarios-insert.sql
@@ -1,3 +1,3 @@
-insert into USUARIOS(id, username, password, role) values(100, 'ana@email.com', '123456', 'ROLE_ADMIN')
-insert into USUARIOS(id, username, password, role) values(101, 'bia@email.com', '123456', 'ROLE_CLIENTE')
-insert into USUARIOS(id, username, password, role) values(102, 'bob@email.com', '123456', 'ROLE_CLIENTE')
+insert into USUARIOS(id, username, password, role) values(100, 'ana@email.com', '$2a$12$jTxvHF6QsBkK/afxxZxYsuJFzJjPVO3zbMukZCxRg1SlcMUbsQWPu', 'ROLE_ADMIN')
+insert into USUARIOS(id, username, password, role) values(101, 'bia@email.com', '$2a$12$jTxvHF6QsBkK/afxxZxYsuJFzJjPVO3zbMukZCxRg1SlcMUbsQWPu', 'ROLE_CLIENTE')
+insert into USUARIOS(id, username, password, role) values(102, 'bob@email.com', '$2a$12$jTxvHF6QsBkK/afxxZxYsuJFzJjPVO3zbMukZCxRg1SlcMUbsQWPu', 'ROLE_CLIENTE')
