e7bbbd3843c5322ee4be998db694bf8a1f11c694
levibezerra <levilimabezerra20@gmail.com>
2024-07-05 18:57:15 -0300

    Testando com autenticação o recurso com permissão de acesso e sem permissão de acesso com status 200 e 403

--- src/test/java/com/levi/demo_park_api/UsuarioIT.java
+++ src/test/java/com/levi/demo_park_api/UsuarioIT.java
@@ -345,10 +345,11 @@ public class UsuarioIT {
     }
 
     @Test
-    public void listarUsuarios_SemQualquerParametro_RetornarListaComStatus200() {
+    public void listarUsuarios_ComUsuarioComPermissao_RetornarListaDeUsuariosComStatus200() {
         List<UsuarioResponseDto> responseBody = testClient
                 .get()
                 .uri("/api/v1/usuarios")
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "ana@email.com", "123456"))
                 .exchange()
                 .expectStatus().isOk()
                 .expectBodyList(UsuarioResponseDto.class)
@@ -356,5 +357,20 @@ public class UsuarioIT {
 
         org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
         org.assertj.core.api.Assertions.assertThat(responseBody.size()).isEqualTo(3);
+    }
+
+    @Test
+    public void listarUsuarios_ComUsuarioSemPermissao_RetornarErrorMessageComStatus403() {
+        ErrorMessage responseBody = testClient
+                .get()
+                .uri("/api/v1/usuarios")
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "bia@email.com", "123456"))
+                .exchange()
+                .expectStatus().isForbidden()
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(403);
     }
 }
