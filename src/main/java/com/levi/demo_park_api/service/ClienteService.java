39b7b39a0441d20d18623c6b2fd017b81fddeeb3
levibezerra <levilimabezerra20@gmail.com>
2024-07-08 23:28:28 -0300

    Adicionando recurso para localizar um cliente pelo identificador

--- src/main/java/com/levi/demo_park_api/service/ClienteService.java
+++ src/main/java/com/levi/demo_park_api/service/ClienteService.java
@@ -2,6 +2,7 @@ package com.levi.demo_park_api.service;
 
 import com.levi.demo_park_api.entity.Cliente;
 import com.levi.demo_park_api.exception.CpfUniqueViolationException;
+import com.levi.demo_park_api.exception.EntityNotFoundException;
 import com.levi.demo_park_api.repository.ClienteRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -22,5 +23,12 @@ public class ClienteService {
         } catch (DataIntegrityViolationException ex) {
             throw new CpfUniqueViolationException(String.format("CPF '%s' não pode ser cadastrado, já existe no sistema", cliente.getCpf()));
         }
+    }
+
+    @Transactional(readOnly = true)
+    public Cliente buscarPorId(Long id) {
+        return clienteRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException(String.format("Cliente id=%s não encontrado no sistema", id))
+        );
     }
 }
--- src/main/java/com/levi/demo_park_api/web/controller/ClienteController.java
+++ src/main/java/com/levi/demo_park_api/web/controller/ClienteController.java
@@ -19,10 +19,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "Clientes", description = "Contém todas as operações relativas ao recurso de um cliente")
 @RequiredArgsConstructor
@@ -56,5 +53,11 @@ public class ClienteController {
         cliente.setUsuario(usuarioService.buscarPorId(userDetails.getId()));
         clienteService.salvar(cliente);
         return ResponseEntity.status(201).body(ClienteMapper.toDto(cliente));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ClienteResponseDto> getById(@PathVariable Long id) {
+        Cliente cliente = clienteService.buscarPorId(id);
+        return ResponseEntity.ok(ClienteMapper.toDto(cliente));
     }
 }
