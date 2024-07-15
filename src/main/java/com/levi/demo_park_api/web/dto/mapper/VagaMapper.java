0e0b0827345ff0ca7a136a06e6fc3cc51f5a91e8
levibezerra <levilimabezerra20@gmail.com>
2024-07-15 10:34:04 -0300

    Adicionando os DTOs e Mapper para Vagas

---
+++ src/main/java/com/levi/demo_park_api/web/dto/mapper/VagaMapper.java
@@ -0,0 +1,21 @@
+package com.levi.demo_park_api.web.dto.mapper;
+
+import com.levi.demo_park_api.entity.Vaga;
+import com.levi.demo_park_api.web.dto.VagaCreateDto;
+import com.levi.demo_park_api.web.dto.VagaResponseDto;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.modelmapper.ModelMapper;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+
+public class VagaMapper {
+    public static Vaga toVaga(VagaCreateDto dto) {
+        return new ModelMapper().map(dto, Vaga.class);
+
+    }
+
+    public static VagaResponseDto tpDto(Vaga vaga) {
+        return new ModelMapper().map(vaga, VagaResponseDto.class);
+    }
+}
