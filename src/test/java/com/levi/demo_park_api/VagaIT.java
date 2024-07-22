f8492edd852d354023e6e931e92eb0f11d698752
levibezerra <levilimabezerra20@gmail.com>
2024-07-21 23:56:26 -0300

    Testando recurso para localizar uma vaga por código com status 200

--- src/test/java/com/levi/demo_park_api/VagaIT.java
+++ src/test/java/com/levi/demo_park_api/VagaIT.java
@@ -75,4 +75,18 @@ public class VagaIT {
                 .jsonPath("method").isEqualTo("POST")
                 .jsonPath("path").isEqualTo("/api/v1/vagas");
     }
+
+    @Test
+    public void buscarVaga_ComCodigoJaExistente_RetornarVagaComStatus200() {
+        testClient
+                .get()
+                .uri("/api/v1/vagas/{codigo}", "A-01")
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "ana@email.com", "123456"))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("id").isEqualTo(10)
+                .jsonPath("codigo").isEqualTo("A-01")
+                .jsonPath("status").isEqualTo("LIVRE");
+    }
 }
