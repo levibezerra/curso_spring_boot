f7e04f1238e8e2850694200b90092c2443a7f41b
levibezerra <levilimabezerra20@gmail.com>
2024-07-25 23:55:37 -0300

    Adicionando a classe EstacionamentoCreateDto

---
+++ src/main/java/com/levi/demo_park_api/web/dto/EstacionamentoCreateDto.java
@@ -0,0 +1,10 @@
+package com.levi.demo_park_api.web.dto;
+
+public class EstacionamentoCreateDto {
+
+    private String placa;
+    private String marca;
+    private String modelo;
+    private String cor;
+    private String clienteCpf;
+}
