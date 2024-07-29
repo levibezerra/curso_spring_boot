a8bb873f0fd01c3bc25eccb42f05bf58502726fe
levibezerra <levilimabezerra20@gmail.com>
2024-07-28 22:50:25 -0300

    Adicionando classes de Repositório e Service para o recurso do estacionamento

---
+++ src/main/java/com/levi/demo_park_api/repository/ClienteVagaRepository.java
@@ -0,0 +1,7 @@
+package com.levi.demo_park_api.repository;
+
+import com.levi.demo_park_api.entity.ClienteVaga;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteVagaRepository extends JpaRepository<ClienteVaga, Long>{
+}
---
+++ src/main/java/com/levi/demo_park_api/service/ClienteVagaService.java
@@ -0,0 +1,20 @@
+package com.levi.demo_park_api.service;
+
+import com.levi.demo_park_api.entity.ClienteVaga;
+import com.levi.demo_park_api.repository.ClienteVagaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+
+public class ClienteVagaService {
+
+    private final ClienteVagaRepository repository;
+
+    @Transactional
+    public ClienteVaga salvar(ClienteVaga clienteVaga) {
+        return repository.save(clienteVaga);
+    }
+}
