6c27e35944545de0382de5446bac98c4572084af
levibezerra <levilimabezerra20@gmail.com>
2024-08-01 12:52:30 -0300

    Adicionando recurso para listar estacionamentos pelo parâmentro CPF

--- src/main/java/com/levi/demo_park_api/service/ClienteVagaService.java
+++ src/main/java/com/levi/demo_park_api/service/ClienteVagaService.java
@@ -3,7 +3,10 @@ package com.levi.demo_park_api.service;
 import com.levi.demo_park_api.entity.ClienteVaga;
 import com.levi.demo_park_api.exception.EntityNotFoundException;
 import com.levi.demo_park_api.repository.ClienteVagaRepository;
+import com.levi.demo_park_api.repository.projection.ClienteVagaProjection;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -31,5 +34,10 @@ public class ClienteVagaService {
     @Transactional(readOnly = true)
     public long getTotalDeVezesEstacionamentoCompleto(String cpf) {
         return repository.countByClienteCpfAndDataSaidaIsNotNull(cpf);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ClienteVagaProjection> buscarTodosPorClienteCpf(String cpf, Pageable pageable) {
+        return repository.findAllByClienteCpf(cpf, pageable);
     }
 }
--- src/main/java/com/levi/demo_park_api/web/controller/EstacionamentoController.java
+++ src/main/java/com/levi/demo_park_api/web/controller/EstacionamentoController.java
@@ -1,11 +1,14 @@
 package com.levi.demo_park_api.web.controller;
 
 import com.levi.demo_park_api.entity.ClienteVaga;
+import com.levi.demo_park_api.repository.projection.ClienteVagaProjection;
 import com.levi.demo_park_api.service.ClienteVagaService;
 import com.levi.demo_park_api.service.EstacionamentoService;
 import com.levi.demo_park_api.web.dto.EstacionamentoCreateDto;
 import com.levi.demo_park_api.web.dto.EstacionamentoResponseDto;
 import com.levi.demo_park_api.web.dto.mapper.ClienteVagaMapper;
+import com.levi.demo_park_api.web.dto.mapper.PageableMapper;
+import com.levi.demo_park_api.web.dto.PageableDto;
 import com.levi.demo_park_api.web.exception.ErrorMessage;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -18,6 +21,10 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -70,6 +77,21 @@ public class EstacionamentoController {
         return ResponseEntity.created(location).body(responseDto);
     }
 
+    @Operation(summary = "Localizar um veículo estacionado", description = "Recurso para retornar um veículo estacionado " +
+            "pelo nº do recibo. Requisição exige uso de um bearer token.",
+            security = @SecurityRequirement(name = "security"),
+            parameters = {
+                    @Parameter(in = ParameterIn.PATH, name = "recibo", description = "Número do recibo gerado pelo check-in")
+            },
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Recurso localizado com sucesso",
+                            content = @Content(mediaType = " application/json;charset=UTF-8",
+                                    schema = @Schema(implementation = EstacionamentoResponseDto.class))),
+                    @ApiResponse(responseCode = "404", description = "Número do recibo não encontrado.",
+                            content = @Content(mediaType = " application/json;charset=UTF-8",
+                                    schema = @Schema(implementation = ErrorMessage.class)))
+            })
+
     @GetMapping("/check-in/{recibo}")
     @PreAuthorize("hasAnyRole('ADMIN', 'CLIENTE')")
     public ResponseEntity<EstacionamentoResponseDto> getByRecibo(@PathVariable String recibo) {
@@ -102,6 +124,16 @@ public class EstacionamentoController {
     public ResponseEntity<EstacionamentoResponseDto> checkout(@PathVariable String recibo) {
         ClienteVaga clienteVaga = estacionamentoService.checkOut(recibo);
         EstacionamentoResponseDto dto = ClienteVagaMapper.toDto(clienteVaga);
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping("/cpf/{cpf}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<PageableDto> getAllEstacionamentosPorCpf(@PathVariable String cpf,
+                                                                   @PageableDefault(size = 5, sort = "dataEntrada",
+                                                                   direction = Sort.Direction.ASC) Pageable pageable) {
+        Page<ClienteVagaProjection> projection = clienteVagaService.buscarTodosPorClienteCpf(cpf, pageable);
+        PageableDto dto = PageableMapper.toDto(projection);
         return ResponseEntity.ok(dto);
     }
 }
